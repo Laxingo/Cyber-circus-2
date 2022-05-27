e235b3530577693a64af8f4ef64d062874d97b57
Laxingo <goncalosilchaves@gmail.com>
2022-05-27 11:23:23 +0100

    POP UPS
    pop

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -4,6 +4,9 @@ onready var slot = $ViewportContainer/Viewport/SlotMachine
 onready var lightAnim = $Luzes/AnimationPlayer
 var rolled = false
 
+var popedup = false
+var popedup2 = false
+
 func _ready():
 	slot.connect("stopped", self, "_on_slot_machine_stopped")
 	lightAnim.play("luz")
@@ -21,4 +24,31 @@ func _on_slot_machine_stopped():
 
 
 func _on_options_button_down():
-	pass # Replace with function body.
+	if popedup== false:
+		$options/AnimationPlayer.play("popup1o")
+		if popedup2 == true:
+			$options/AnimationPlayer.play("popup2c")
+			popedup2 = false
+			popedup = false
+		else:
+			popedup = true
+			popedup2 = false
+	elif popedup == true:
+		$options/AnimationPlayer.play("popup1c")
+		popedup = false
+		popedup2 = false
+
+func _on_replay_button_down():
+	if popedup2== false:
+		$options/AnimationPlayer.play("popup2o")
+		if popedup == true:
+			$options/AnimationPlayer.play("popup1c")
+			popedup2 = false
+			popedup = false
+		else:
+			popedup2 = true
+			popedup = false
+	elif popedup2 == true:
+		$options/AnimationPlayer.play("popup2c")
+		popedup2 = false
+		popedup = false
